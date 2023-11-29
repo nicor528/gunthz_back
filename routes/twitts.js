0e1f0324b7357a6abb63022a8c9709e8b07fe797
nicor528 <nicolasriquelme528@gmail.com>
2023-11-29 15:30:17 -0300

    28-11-23

--- routes/twitts.js
+++ routes/twitts.js
@@ -232,8 +232,8 @@ router.post("/tredingTwitts", async (req, res) => {
                         //console.log(newTwitts)
                         getAllTwitts2(newTwitts).then(cleanTwitts => {
                             console.log(cleanTwitts)
-                            updateTwittsLinks2(cleanTwitts).then(twittis => {
-                                res.status(200).send({status: true, message: "ok", key: newKey, data: twittis})
+                            updateTwittsLinks2(newTwitts.twitts).then(twitts => {
+                                res.status(200).send({status: true, message: "ok", key: newKey, data: twitts})
                             }).catch(error => {res.status(400).send({error, status: false})})
                         }).catch(error => {res.status(400).send({error, status: false})})
                     }).catch(error => {res.status(400).send({error, status: false})})
