f49019223f0496da0082026facc0492e43694261
nicor528 <nicolasriquelme528@gmail.com>
2024-01-18 13:53:03 -0300

    18-1-24

--- routes/mubert.js
+++ routes/mubert.js
@@ -130,7 +130,9 @@ router.post("/create-song", (req, res) => {
                         saveSong(id, song, prompt).then(path => {
                             console.log("test2")
                             addTwitt(id, prompt, path, user.name + " " + user.lastName, user.profilePicture, "song").then(() => {
-                                res.status(200).send({status: true, message: "ok", key: newKey})
+                                generarEnlaceDeDescarga(path).then(link => {
+                                    res.status(200).send({status: true, message: "ok", key: newKey, data: link})
+                                }).catch(error => {res.status(400).send({error, status: false})})
                             }).catch(error => {res.status(400).send({error, status: false})})
                         }).catch(error => {console.log("asdasd"),res.status(400).send({error, status: false})})
                     }).catch(error => {console.log("testx"),res.status(400).send({error, status: false})})
