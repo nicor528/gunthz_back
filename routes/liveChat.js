d2e432567739fd032bfecc9714cf7a3193cb8e63
nicor528 <nicolasriquelme528@gmail.com>
2024-01-26 18:23:57 -0300

    1-26-24

--- routes/liveChat.js
+++ routes/liveChat.js
@@ -27,7 +27,7 @@ router.get("/getLast50LiveChat", async (req, res) => {
     if(token){
         verifyToken(token).then(id => {
             getLast50LiveChat(index).then(chat => {
-                res.status(200).send({data: chat, status: true, message: "succefull"})
+                res.status(200).send({data: {index: index, chat}, status: true, message: "succefull"})
             }).catch(error => {res.status(400).send({error, status: false})})
         }).catch(error => {res.status(400).send({error, status: false})})
     }else{
